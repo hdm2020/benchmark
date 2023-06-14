fc9f8fd084d96cf81218ddffa8b00f46baac9484
hdm2020 <71557587+hdm2020@users.noreply.github.com>
2023-06-14 20:54:14 +0800

    Update testdata.merge.R

--- scripts/atSNP/testdata.merge.R
+++ scripts/atSNP/testdata.merge.R
@@ -1,12 +1,12 @@
 #merge experimental and predictive difference value(2 alleles off snp) of TF binding
 
 #read evaluation data
+evaldatafile<-'../snpdata/testdata/GVAT_novelbatch_TFSYMBOL.txt'
 evaldata<-read.table(evaldatafile,sep='\t',header=T,stringsAsFactors = F)
 evaldata<-evaldata[,c('snp','pbs','pval','TF_SYMBOL')]
 
 #JASPAR 2022
 modeltf<-'./testdata/evaldata_interatSNP.jaspar587tf.csv'
-evaldatafile<-'../snpdata/testdata/GVAT_novelbatch_TFSYMBOL.txt'
 predfile<-'./testdata/pwm_jaspar2022/results/atSNP.jaspar2022.txt'
 outfile<-'./testdata/atSNP.jaspar2022.merged.expe.pred.results.txt'
 
@@ -24,7 +24,6 @@ write.table(df,outfile,col.names=T,row.names=F,quote=F,sep='\t')
 
 #HOCOMOCO v11
 modeltf<-'./testdata/evaldata_interatSNP.hocomoco401tf.csv'
-evaldatafile<-'../snpdata/testdata/GVAT_novelbatch_TFSYMBOL.txt'
 predfile<-'./testdata/pwm_hocomocov11/results/atSNP.hocomocov11.txt'
 outfile<-'./testdata/atSNP.hocomocov11.merged.expe.pred.results.txt'
 
@@ -38,4 +37,3 @@ result<-merge(result,model_tf)
 #merge and output
 df<-merge(evaldata,result,by=c('snp','TF_SYMBOL'))
 write.table(df,outfile,col.names=T,row.names=F,quote=F,sep='\t')
-
