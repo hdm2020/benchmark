452bff8b5cd13864d408b0adf708a5cfa8e0dbf4
hdm2020 <71557587+hdm2020@users.noreply.github.com>
2023-06-14 21:32:16 +0800

    Update testdata.merge.R

--- scripts/tRap/testdata.merge.R
+++ scripts/tRap/testdata.merge.R
@@ -1,12 +1,12 @@
 #merge experimental and predictive difference value(2 alleles off snp) of TF binding
 
 #read evaluation data
+evaldatafile<-'../snpdata/testdata/GVAT_novelbatch_TFSYMBOL.txt'
 evaldata<-read.table(evaldatafile,sep='\t',header=T,stringsAsFactors = F)
 evaldata<-evaldata[,c('snp','pbs','pval','TF_SYMBOL')]
 
 #JASPAR 2022
 modeltf<-'./testdata/evaldata_intertRap.jaspar587tf.csv'
-evaldatafile<-'../snpdata/testdata/GVAT_novelbatch_TFSYMBOL.txt'
 predfile<-'./testdata/pwm_jaspar2022/results/tRap.jaspar2022.txt'
 outfile<-'./testdata/tRap.jaspar2022.merged.expe.pred.results.txt'
 
@@ -15,7 +15,7 @@ model_tf<-read.csv(modeltf,stringsAsFactors=F)
 result<-read.table(predfile,header=T,sep='\t',stringsAsFactors=F)
 result<-result[,c(1,3:11)]
 result<-result[result$seq1!='seq1',]
-colnames(result)[,1:2]<-c('snp','model_name')
+colnames(result)[1:2]<-c('snp','model_name')
 result$snp<-gsub('_WT','',result$snp)
 result<-merge(result,model_tf)
 
@@ -26,7 +26,6 @@ write.table(df,outfile,col.names=T,row.names=F,quote=F,sep='\t')
 
 #HOCOMOCO v11
 modeltf<-'./testdata/evaldata_intertRap.hocomoco401tf.csv'
-evaldatafile<-'../snpdata/testdata/GVAT_novelbatch_TFSYMBOL.txt'
 predfile<-'./testdata/pwm_hocomocov11/results/tRap.hocomocov11.txt'
 outfile<-'./testdata/tRap.hocomocov11.merged.expe.pred.results.txt'
 
@@ -35,7 +34,7 @@ model_tf<-read.csv(modeltf,stringsAsFactors=F)
 result<-read.table(predfile,header=T,sep='\t',stringsAsFactors=F)
 result<-result[,c(1,3:11)]
 result<-result[result$seq1!='seq1',]
-colnames(result)[,1:2]<-c('snp','model_name')
+colnames(result)[1:2]<-c('snp','model_name')
 result$snp<-gsub('_WT','',result$snp)
 result<-merge(result,model_tf)
 
