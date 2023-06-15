be5ec3b527905d617e1ebefbd38813bd3e166d7c
hdm2020 <71557587+hdm2020@users.noreply.github.com>
2023-06-15 11:40:35 +0800

    Update test.sh

--- scripts/tRap/test.sh
+++ scripts/tRap/test.sh
@@ -63,7 +63,7 @@ Rscript testdata.merge.R #note: you need to change the code for your data
 
 #8.1 JASPAR 2022
 Rscript --vanilla auroc_auprc.R -e ../snpdata/testdata/evaldata -f $datadir/tRap.jaspar2022.merged.expe.pred.results.txt -m $datadir/evaldata_intertRap.jaspar587tf.csv -d log.ratio -o $datadir/tRap.jaspar2022.tf.roc.prc.txt
-mv besttfmodel.roc.prc.txt tRap.jaspar2022.besttfmodel.roc.prc.txt
+mv besttfmodel.roc.prc.txt $datadir/tRap.jaspar2022.besttfmodel.roc.prc.txt
 #input for the script: -e: prefix of positive set and negative set,eg: evaldata_positive_data.txt,evaldata_negative_data.txt .The 'snp' and 'TF_SYMBOL' columns must be provided.
 #-f: a file of merged experimental and predictive difference value(2 alleles of snp) of TF binding, the 'snp','TF_SYMBOL','model_name',predictive difference value of TF binding colums must be provided.
 #-d: predictive difference value(2 alleles of snp) of TF binding
