9bc0fa422deaaed7d91068ee9b73cd3bf7fed806
hdm2020 <71557587+hdm2020@users.noreply.github.com>
2023-09-27 21:37:25 +0800

    Update testdata.merge.R

--- scripts/Enformer/testdata.merge.R
+++ scripts/Enformer/testdata.merge.R
@@ -1,7 +1,7 @@
 #merge experimental and predictive difference value(2 alleles off snp) of TF binding
 
 modeltf<-'./testdata/evaldata_interenformer2131tf.csv'
-evaldatafile<-'../snpdata/testdata/GVAT_novelbatch_TFSYMBOL.txt'
+evaldatafile<-'../../snpdata/testdata/GVAT_novelbatch_TFSYMBOL.txt'
 predfile<-'./testdata/results/Enformer.txt'
 outfile<-'./testdata/Enformer.merged.expe.pred.results.txt'
 
